98214e5e1ac23002bde2b2accab38f1d87b692da
Szabo Bogdan <szabobogdan@yahoo.com>
2015-08-21 13:39:30 +0300

    Test refactoring

--- test/api.js
+++ test/api.js
@@ -1,16 +1,15 @@
-var assert = require('assert');
 var should = require('should');
 var Calais = require('../lib/calais').Calais;
 
-var some_api_key = '';
+var someApiKey = '';
 
-describe("Calais fetch", function() {
+describe('Calais fetch', function() {
   it('should return 401 if no api key is provided', function(done) {
     this.timeout(5000);
 
     var calais = new Calais();
 
-    calais.fetch(function(err, data) {
+    calais.fetch(function(err) {
       should.exist(err);
       done();
     });
@@ -19,11 +18,11 @@ describe("Calais fetch", function() {
   it('should return 200', function(done) {
     this.timeout(5000);
 
-    var calais = new Calais(some_api_key, {
+    var calais = new Calais(someApiKey, {
       content: 'Microsoft Apple Google San-Francisco New York Cluj-Napoca'
     });
 
-    calais.fetch(function(err, data) {
+    calais.fetch(function(err) {
       if(err) {
         return done(err);
       }
--- test/options.js
+++ test/options.js
@@ -1,16 +1,15 @@
-var assert = require('assert');
 var Calais = require('../lib/calais').Calais;
 
-var some_api_key = 'KEY';
+var someApiKey = 'KEY';
 
-describe("Calais options", function() {
+describe('Calais options', function() {
   it('should set the api key', function () {
-    var calais = new Calais(some_api_key);
-    calais.apiKey.should.equal(some_api_key);
+    var calais = new Calais(someApiKey);
+    calais.apiKey.should.equal(someApiKey);
   });
 
   it('should set option', function () {
-    var calais = new Calais(some_api_key, {'language': 'French'});
+    var calais = new Calais(someApiKey, {'language': 'French'});
     calais.options.language.should.equal('French');
   });
 });
